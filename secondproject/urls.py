c32850371bc013bbb3d2ed6b9ec002e7c09dbfae
yonsoo <whatesloo@likelion.org>
2019-03-07 17:11:46 +0900

    first blog

---
+++ secondproject/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+import blog.views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', blog.views.blog, name='blog'),
+
+    path('blog/', include('blog.urls')),
+    path('portfolio/', include('portfolio.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
